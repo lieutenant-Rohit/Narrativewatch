d503f847deb223b75098473657017a7340ce8e4a
Rohit <rohitbishtt69@gmail.com>
2026-06-22 18:04:11 +0530

    Project setup

---
+++ src/main/java/com/narrativewatch/NarrativewatchApplication.java
@@ -0,0 +1,13 @@
+package com.narrativewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarrativewatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarrativewatchApplication.class, args);
+    }
+
+}
